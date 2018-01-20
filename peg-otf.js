2c4af9122c3d548982ff630a4c8c33c78640b7bd
oanylund <ole.a.nylund@gmail.com>
2018-01-20 18:34:31 +0100

    updated manual test script for grammar

--- peg-otf.js
+++ peg-otf.js
@@ -1,29 +1,17 @@
 const PEG = require("pegjs");
 const { readFileSync } = require("fs");
-const { buildList } = require("./src/util");
+const parse = require('./src/parse');
 
-const parser = PEG.generate(
-    readFileSync(__dirname + "/grammar/grammar.1.pegjs", "utf8"),
-    { optimize: "size" }
-);
+const prettyPrint = print => console.log(JSON.stringify(print, null, 2));
 
-const parse = parser.parse;
-
-const skuldWells = parse("{(71-74),(76-79),(81-84)}");
-const sys19 = ["19"];
-
-const list = buildList([sys19, skuldWells]);
-
-console.log(skuldWells)
-
-
-// console.log(
-//     JSON.stringify(
-//         parser.parse(
-//             readFileSync(
-//                 __dirname+"/test.txt", 
-//                 "utf8"
-//             )
-//         )
-//         , null, 2)
+// const parser = PEG.generate(
+//   readFileSync(__dirname + "/grammar/grammarWithRefs.pegjs", "utf8"),
+//   { optimize: "size" }
 // );
+
+// const parse = parser.parse;
+
+const testP = parse("{(71-74),(76-79),(81-84)}");
+
+prettyPrint(testP);
+
