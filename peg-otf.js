9fdf08756eab6feb47c5027061043a7e67e6357d
oanylund <ole.a.nylund@gmail.com>
2018-03-26 15:22:15 +0200

    deleted old manual test file for parser

--- peg-otf.js
+++
@@ -1,7 +0,0 @@
-const parser = require("./dist/left-expand-pattern-parser.cjs");
-
-const prettyPrint = print => console.log(JSON.stringify(print, null, 2));
-
-const parsedOut = parser("{¤(71-74),(76-79),(81-84)}");
-
-prettyPrint(parsedOut);
